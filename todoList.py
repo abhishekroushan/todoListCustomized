d5f6a3214a27552aabb0c128db368a193be32289
abhishekroushan <abhishek.roushan12@gmail.com>
2023-01-08 22:11:54 -0800

    add attributes to create todo list dummy

--- todoList.py
+++ todoList.py
@@ -17,11 +17,21 @@ class Attributes(object):
         return self.d[key]
     def addKeyValue(self, key, value):
         self.d[key]=value
+    def serialize(self):
+        result = "{"
+        for k,v in self.d.items():
+            result+=k+":"+str(v)+","
+        result+="}"
+        return result
 
 class TodoListItem(object):
-    def __init__(self, item:str):
+    def __init__(self, item:str, attributes=Attributes({})):
         self.item = item
-        self.attributes = None
+        self.attributes = attributes
+    def getItem(self):
+        return self.item
+    def getAttributes(self):
+        return self.attributes.serialize()
     def reset(self):
         self.item = None
     def setAttributes(self, attributes):
@@ -40,5 +50,21 @@ class TodoList(object):
     def print(self):
         print("Todo List:")
         for i,l in enumerate(self.list):
-            print(i,":",l)
+            print(i,":", l.getItem(), "--", l.getAttributes())
 
+def createTodoListDummy():
+    tl = TodoList()
+    tl.addTodoListItem(TodoListItem("buy groceries", 
+                       Attributes({"priority":1, "time required":2, "impact":8.5})))
+    tl.addTodoListItem(TodoListItem("workout",
+                       Attributes({"priority":2, "time required":3.5, "impact":4.5})))
+    tl.addTodoListItem(TodoListItem("finish L1 cache implementation",
+                       Attributes({"priority":3, "time required":1.5, "impact":1.5})))
+    tl.addTodoListItem(TodoListItem("study Freakonomics",
+                       Attributes({"priority":4, "time required":1, "impact":1})))
+    tl.print()
+
+def main():
+    createTodoListDummy()
+
+main()
